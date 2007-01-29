198c790659a66dfe9c8414b9b500b996d881a688
Ray Brownrigg <Ray.Brownrigg@mcs.vuw.ac.nz>
2007-01-29 14:21:49 +0000

    version 2.0-33

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .First.lib <- function(lib, pkg) {
-  # minka: only do this if R_MAP_DATA_DIR doesn't exist?
+  if(!exists("Sys.setenv", envir = baseenv()))
+    Sys.setenv <- Sys.putenv
   if (Sys.getenv("R_MAP_DATA_DIR") == "")
-    Sys.putenv("R_MAP_DATA_DIR"=paste(lib, pkg, "mapdata/", sep="/"))
+    Sys.setenv("R_MAP_DATA_DIR"=paste(lib, pkg, "mapdata/", sep="/"))
   library.dynam("maps", pkg, lib)
 }
