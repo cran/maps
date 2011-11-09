1d099e4e237bd47ac71cedf3078ac198958d05fb
Ray Brownrigg <Ray.Brownrigg@ecs.vuw.ac.nz>
2011-11-09 00:00:00 +0000

    version 2.2-2

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,4 @@
 .onLoad <- function(lib, pkg) {
-  if(!exists("Sys.setenv", envir = baseenv()))
-    Sys.setenv <- Sys.putenv
   if (Sys.getenv("R_MAP_DATA_DIR") == "")
     Sys.setenv("R_MAP_DATA_DIR"=paste(lib, pkg, "mapdata/", sep="/"))
   library.dynam("maps", pkg, lib)
